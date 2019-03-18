ddfe85170ea7941670ad197843b68cc9fc47f99b
Ravel <raveladamony@outlook.fr>
2019-03-18 20:18:47 +0100

    Started to add translation tags.

--- Widgets/GlobalTabWidget/globaltabwidget.cpp
+++ Widgets/GlobalTabWidget/globaltabwidget.cpp
@@ -6,15 +6,15 @@ GlobalTabWidget::GlobalTabWidget(QWidget *parent)
 {
     //Add bunker tab
     m_bunker_widget = new BunkerWidget(this);
-    addTab(m_bunker_widget, "Bunker");
+    addTab(m_bunker_widget, tr("Bunker"));
 
     //Add MC tab
     m_mc_widget = new MCWidget(this);
-    addTab(m_mc_widget, "MC");
+    addTab(m_mc_widget, tr("MC"));
 
     //Add Nightclub tab
     m_nightclub_widget = new NightclubWidget(this);
-    addTab(m_nightclub_widget, "Nightclub");
+    addTab(m_nightclub_widget, tr("Nightclub"));
 }
 
 //Destructor
